8bda75cc27ced0ca8837b0d657e706a090c012d8
Lucas Azevedo <lucasazevedo@vastaweb.com>
2024-03-30 16:49:55 -0300

    add

---
+++ src/pages/cart.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Cart: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Cart</title>
+        <meta name="description" content="Cart page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        <h1>Cart</h1>
+
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint error
+          consectetur modi odio obcaecati tempore dignissimos assumenda, eum
+          reiciendis dolores magni autem at debitis voluptas. Rem ipsa deleniti
+          autem dignissimos.
+        </p>
+    </>
+  );
+};
+
+export default Cart;
---
+++ src/pages/products.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Products: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Products</title>
+        <meta name="description" content="Products page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        <h1>Products</h1>
+
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint error
+          consectetur modi odio obcaecati tempore dignissimos assumenda, eum
+          reiciendis dolores magni autem at debitis voluptas. Rem ipsa deleniti
+          autem dignissimos.
+        </p>
+    </>
+  );
+};
+
+export default Products;
